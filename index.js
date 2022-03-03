4c2257a82628acae9c2492318968f14e50574267
Jordan Oetting <JOetting71@gmail.com>
2022-03-02 23:00:55 -0500

    / route

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
+console.log(__dirname);
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.listen(PORT, ()=>{console.log('app is listening on port 3000')})
