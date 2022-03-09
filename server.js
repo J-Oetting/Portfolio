cd9fc9431fd433c65810f01f9904d8b21735463d
Jordan Oetting <JOetting71@gmail.com>
2022-03-08 21:35:08 -0500

    fixed bundle 404 error, express static

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
@@ -6,5 +7,7 @@ const PORT = 3000;
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 })
+
+app.use(express.static(path.join(__dirname)));
 
 app.listen(PORT, ()=>{console.log('app is listening on port 3000')})
